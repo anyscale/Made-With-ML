47c3c29777729c2490e5a0c581f6cfc1745499e4
Kai Fricke <kai@anyscale.com>
2023-05-17 12:11:40 +0100

    job update

--- anyscale/job_train.sh
+++ anyscale/job_train.sh
@@ -4,6 +4,8 @@ DATASET_LOC="https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/da
 HOLDOUT_LOC="https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/datasets/madewithml/holdout.csv"
 TRAIN_LOOP_CONFIG="{'dropout_p': 0.5, 'lr': 1e-4, 'lr_factor': 0.8, 'lr_patience': 3}"
 
+pip install -e .
+
 python ./src/madewithml/train.py llm \
     "$DATASET_LOC" \
     "$TRAIN_LOOP_CONFIG" \
