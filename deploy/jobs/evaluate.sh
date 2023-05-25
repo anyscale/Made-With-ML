cd40e73afd383c24390f4508490065b3e4a53be0
GokuMohandas <gokumd@gmail.com>
2023-05-25 11:55:12 -0700

    testing deployment templates

--- deploy/jobs/evaluate.sh
+++ deploy/jobs/evaluate.sh
@@ -2,7 +2,6 @@
 
 # Evaluate model
 set -xe
-git pull origin $branch
 RUN_ID=$(python -c "from madewithml.predict import get_best_run_id as g; print(g('$experiment_name', 'val_loss', 'ASC'))")
 HOLDOUT_LOC="https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/datasets/madewithml/holdout.csv"
 python src/madewithml/evaluate.py \
