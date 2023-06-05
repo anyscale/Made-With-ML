23b40df92f77b0457a5750d8f824e89c369759b0
GokuMohandas <gokumd@gmail.com>
2023-06-05 14:25:17 -0700

    back to prod

--- deploy/jobs/test_model.sh
+++ deploy/jobs/test_model.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:$PWD
 
+# Workaround for /mnt/user_storage: Download mlflow from S3
+MODEL_REGISTRY=$(python -c "from madewithml.config import MODEL_REGISTRY; print(str(MODEL_REGISTRY))")
+aws s3 sync $S3_BUCKET/$GITHUB_USERNAME/mlflow "$MODEL_REGISTRY"
+
 # Get run ID
 if [[ -z "${RUN_ID}" ]]; then  # if RUN_ID is set use it, else get the best run
     RUN_ID=$(python madewithml/predict.py get-best-run-id --experiment-name $EXPERIMENT_NAME --metric val_loss --mode ASC)
