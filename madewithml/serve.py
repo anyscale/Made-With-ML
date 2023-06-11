e8089bed36c25b97e902c891d5090f436101c9c8
GokuMohandas <gokumd@gmail.com>
2023-06-10 22:45:11 -0700

    updated README

--- madewithml/serve.py
+++ madewithml/serve.py
@@ -78,5 +78,5 @@ if __name__ == "__main__":
     parser.add_argument("--run_id", help="run ID to use for serving.")
     parser.add_argument("--threshold", type=float, default=0.9, help="threshold for `other` class.")
     args = parser.parse_args()
-    ray.init(address="auto")
+    ray.init()
     serve.run(ModelDeployment.bind(run_id=args.run_id, threshold=args.threshold))
